840d4670184f369cf1602b0274f25772bb19d712
rida485 <redha.algeriano5@gmail.com>
2024-07-13 16:13:33 +0200

    page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,7 @@
 
 
 import BentoGrid from "@/components/sections/bentogrid";
-import Features from "@/components/sections/features";
 import HeroLanding from "@/components/sections/hero-landing";
-import InfoLanding from "@/components/sections/info-landing";
 import Powered from "@/components/sections/powered";
 import PreviewLanding from "@/components/sections/preview-landing";
 import Lenis from "lenis";
